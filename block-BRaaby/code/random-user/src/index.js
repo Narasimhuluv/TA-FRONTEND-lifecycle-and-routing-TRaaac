181df650cf62a8be5d0b1413ffa6bf0613428980
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-09-17 23:26:07 +0530

    add get random user file

---
+++ block-BRaaby/code/random-user/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/style.css';
+import './stylesheets/custom.css'
+import App from './Components/App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
